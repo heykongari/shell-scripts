254c491e99b2f3e5a1f1ea17ba23b3ee3256433e
sagar kongari <sagarkongariwork@gmail.com>
2025-07-30 12:21:53 +0000

    aws-ec2 script update

--- aws-ec2/config.sh
+++ aws-ec2/config.sh
@@ -3,6 +3,6 @@
 export AMI_ID="ami-020cba7c55df1f615"
 export INSTANCE_TYPE="t2.micro"
 export INSTANCE_COUNT="1"
-export KEY_NAME="aws-key"
-export SG_ID="sg-0364395c9405f510a"
+export KEY_NAME=""
+export SG_ID=""
 export REGION="us-east-1"
