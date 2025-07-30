b06795f424717be4bf3e912bdca93cc3e560b496
sagar kongari <sagarkongariwork@gmail.com>
2025-07-30 12:25:06 +0000

    aws-ec2 script update

--- aws-ec2/config.sh
+++ aws-ec2/config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# AMI ID of Ubuntu 24.04 OS
 export AMI_ID="ami-020cba7c55df1f615"
+
+# other variables
 export INSTANCE_TYPE="t2.micro"
 export INSTANCE_COUNT="1"
 export KEY_NAME=""
